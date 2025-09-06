f6c88a3568b27d2deb8cc305dd62e6bfe20841cd
Ansh2708 <anshtripathi220@gmail.com>
2025-09-06 23:18:47 +0530

    feat: Add SSL configuration for database connection

--- models/index.js
+++ models/index.js
@@ -1,46 +1,47 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+// This is the main connection setup.
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    port: process.env.DB_PORT, // We need the port here
+    dialect: 'mysql',
+    // --- THIS IS THE CRUCIAL NEW PART ---
+    // This tells Sequelize to use an encrypted connection.
+    dialectOptions: {
+      ssl: {
+        // This is not strictly required by Railway, but it's good practice.
+        // It tells the connection to fail if the certificate is invalid.
+        rejectUnauthorized: true 
+      }
+    }
   }
 );
 
+// The rest of your file (loading models, defining relationships) remains the same.
 const db = {};
-
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Loading all our models, including the new ones for badges.
 db.users = require('./user.js')(sequelize);
 db.courses = require('./course.js')(sequelize);
 db.lessons = require('./lesson.js')(sequelize);
 db.userProgress = require('./userProgress.js')(sequelize);
-db.badges = require('./badge.js')(sequelize); 
+db.badges = require('./badge.js')(sequelize);
 db.userBadges = require('./userBadge.js')(sequelize);
 
-// --- Defining all relationships ---
-
-// A Course has many Lessons.
+// Defining all relationships
 db.courses.hasMany(db.lessons, { foreignKey: 'courseId' });
 db.lessons.belongsTo(db.courses, { foreignKey: 'courseId' });
-
-// A User can complete many Lessons (through the UserProgress table).
 db.users.belongsToMany(db.lessons, { through: db.userProgress });
 db.lessons.belongsToMany(db.users, { through: db.userProgress });
-
-// --- NEW: Badge relationships ---
-// A User can have many Badges (through the UserBadges table).
 db.users.belongsToMany(db.badges, { through: db.userBadges });
-// A Badge can be earned by many Users (through the UserBadges table).
 db.badges.belongsToMany(db.users, { through: db.userBadges });
 
-// This command syncs our database, creating the new tables if they don't exist.
 db.sequelize.sync({ force: false })
   .then(() => {
     console.log('✅ Success! Database and all tables have been synced.');
